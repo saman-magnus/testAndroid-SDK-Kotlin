fd8d484c4930281837bab30c28207aab7c2f9eb0
Alireza Bashi <52131452+alirezabashi98@users.noreply.github.com>
2025-01-18 16:11:22 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,40 +1,30 @@
-import com.android.sdklib.AndroidVersion.VersionCodes
-
 plugins {
     kotlin("plugin.serialization") version "2.0.0"
-    id("com.android.library") version "8.5.2"
+    id("com.android.library") version "8.8.0"
     id("org.jetbrains.kotlin.android") version "1.9.0"
     id("maven-publish")
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = "com.zarinpal"
-            artifactId = "zarinPal-library"
-            // Whenever the version is updated, it should be updated in the following path:
-            // java/com/example/zarinpal/data/remote/PaymentService.kt
-            // header("User-Agent", "ZarinPalSdk/v.1.0.0 (android kotlin)")
-            version = "1.0.0"
-
-            afterEvaluate {
-                from(components["release"])
-            }
-        }
-    }
-}
+val ktor_version = "1.6.3"
 
 android {
     namespace = "com.example.zarinpal"
     compileSdk = 34
 
-    defaultConfig {
-        minSdk = 24
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
 
+    defaultConfig {
+        aarMetadata {
+            minCompileSdk = 24
+        }
+        minSdk = 24
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
-
 
     buildTypes {
         release {
@@ -54,33 +44,64 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+}
 
-    publishing {
-        singleVariant("release")
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.zarinpal"
+            artifactId = "zarinPal-library"
+            version = "1.0.0"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+            artifact("$buildDir/outputs/aar/zarinPal-library-release.aar")
+
+            pom {
+                dependencies {
+                    implementation("io.ktor:ktor-client-core:$ktor_version")
+                    implementation("io.ktor:ktor-client-android:$ktor_version")
+                    implementation("io.ktor:ktor-client-serialization:$ktor_version")
+                    implementation("io.ktor:ktor-client-logging:$ktor_version")
+                    implementation("ch.qos.logback:logback-classic:1.2.3")
+                    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri(layout.buildDirectory.dir("repo"))
+        }
     }
 }
 
 dependencies {
-    // Ktor
-    val ktor_version = "1.6.3"
-
-    api("io.ktor:ktor-client-core:$ktor_version")
-    api("io.ktor:ktor-client-android:$ktor_version")
-    api("io.ktor:ktor-client-serialization:$ktor_version")
-    api("io.ktor:ktor-client-logging:$ktor_version")
-
-    api("ch.qos.logback:logback-classic:1.2.3")
-
-    api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
-    api("androidx.core:core-ktx:1.13.0")
-}
-
-tasks.register<Jar>("createJar") {
-    archiveBaseName.set("zarinPal-library")
-    archiveVersion.set("1.0.0")
-
-    manifest {
-        attributes["Implementation-Title"] = "zarinPal-library"
-        attributes["Implementation-Version"] = "1.0.0"
+    api("io.ktor:ktor-client-core:$ktor_version") {
+        isTransitive = true
     }
+    api("io.ktor:ktor-client-android:$ktor_version") {
+        isTransitive = true
+    }
+    api("io.ktor:ktor-client-serialization:$ktor_version") {
+        isTransitive = true
+    }
+    api("io.ktor:ktor-client-logging:$ktor_version") {
+        isTransitive = true
+    }
+    api("ch.qos.logback:logback-classic:1.2.3") {
+        isTransitive = true
+    }
+    api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0") {
+        isTransitive = true
+    }
+    implementation("androidx.core:core-ktx:1.13.0")
 }
+
+tasks.register<Copy>("copyLibs") {
+    from(configurations.getByName("implementation"))
+    into("libs")
+}
+
