4b9c38c43cbd8987b7a2621bd8053ca31b500abb
phoenix marie <phoenixmariepornstaractress@gmail.com>
2025-04-08 11:22:59 +0330

    Update GraphTransactionModel.kt
    Making better

--- src/main/java/com/example/zarinpal/data/remote/dto/transaction/GraphTransactionModel.kt
+++ src/main/java/com/example/zarinpal/data/remote/dto/transaction/GraphTransactionModel.kt
@@ -1,17 +1,16 @@
 package com.example.zarinpal.data.remote.dto.transaction
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 /**
- * Represents the GraphQL request for fetching transactions.
+ * Represents a GraphQL request used to fetch transaction data.
  *
- * @property query The GraphQL query string to be executed for the transaction operation.
- * @property variables Contains the variables required for the transaction operation,
- * including terminalId, filter, limit, offset, and token.
+ * @property query The GraphQL query string to be executed.
+ * @property variables The set of parameters required for retrieving transactions,
+ * such as terminalId, filter criteria, pagination limits, and authentication token.
  */
 @Serializable
 data class GraphTransactionModel(
-    val query :String,
+    val query: String,
     val variables: TransactionRequest
 )
