26a6c619e112104b8ff08c3d6dd0ceed52527286
MagNus <41460065+saman-magnus@users.noreply.github.com>
2025-06-01 17:02:47 +1000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("plugin.serialization") version "2.0.0"
     id("com.android.library") version "8.8.0"
-    id("org.jetbrains.kotlin.android") version "1.9.0"
+    id("org.jetbrains.kotlin.android") version "2.1.0"
     id("maven-publish")
     id("org.jetbrains.dokka") version "1.8.20"
 }
